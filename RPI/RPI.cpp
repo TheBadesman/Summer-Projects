1277c92e141844b2978bd038b508d3c2bd8bf6e3
TheBadesman <Cameron@ottleys.net>
2025-05-25 15:24:40 -0600

    1.00
    This version of the program only has the ability to give the input as text, next version will have the ability to give the input as a file

--- RPI/RPI.cpp
+++ RPI/RPI.cpp
@@ -10,58 +10,126 @@ input from the user and uses it to rate a rank players
 #include <string>
 #include <vector>
 #include <cctype>
+#include <algorithm>
 
-//namespace
-using namespace std;
+using namespace std; //namespace
 
-//struct to make storing the infomation about the teams easier
-struct teams
+struct teams //struct to make storing the infomation about the teams easier
 {
     string name;
     int wins;
     int loses;
+    double games;
+    float record;
+    float schStr;
+    float oppStr;
 
-    int total_games(){
-        return wins + loses;
+    void totalGames(){
+        games = wins + loses;
     }
 
-    float winloss(){
-        return static_cast<double>(wins) / total_games();
+    void winLoss(){
+        record = wins / games;
     }
-
 };
+
+struct weight //struct to see the weight of each area
+{
+    float winWeight;
+    float strength;
+    float opponents;
+};
+
 
 
 int main()
 {
     //the vector to hold all the teams in the league and the string to hold the ints before we convert them
     vector<teams> league;
+    weight percent;
     string convertor;
-    string finished;
+    double str;
 
-    cout << "Enter in all the information asked. When there are no more teams to add, enter done.\n";
+    //Gets input form the user to know the weight of each category
+    while (true) {
+        cout << "Enter the weight of each category as a whole number\n\n";
 
-    while (true)
-    {
-        //a vector to hold the team while we grab all the needed input
-        teams holder;
+        cout << "Win/Loss: "; getline(cin, convertor); percent.winWeight = stof(convertor) / 100;
+        cout << "Strength of their schedule: "; getline(cin, convertor); percent.strength = stof(convertor) / 100;
+        cout << "Strength of their opponents schedule: "; getline(cin, convertor); percent.opponents = stof(convertor) / 100;
+
+        //Printing a buffer to help with readability
+        cout << "-------------------------";
+
+        if (percent.winWeight + percent.strength + percent.opponents == 1) { break; }
+        
+        cout << "Sorry, the weight you added does not equal 100. Please try again\n";
+    }
+
+    cout << "\nEnter in all the information asked. When there are no more teams to add, enter done.\n";
+
+    while (true) {
+        teams holder; //a vector to hold the team while we grab all the needed input
         
         //Getting the name, and checking if it is the last one
-        cout << "Enter the teams name: ";
-
-        getline(cin, holder.name); cout << '\n';
-
+        cout << "\nEnter the teams name (Or done if you are finished): "; getline(cin, holder.name);
         for (char &c : holder.name) {c = toupper(c);}
-
-        if (finished == "done") { break; }
+        if (holder.name == "DONE") { cout << "\n-------------------------\n"; break; }
 
         //Getting the number of wins and losses, then convertering them to ints
-        cout << "Enter the number of wins: "; getline(cin, convertor); cout << '\n'; holder.wins = stoi(convertor);
-        cout << "Enter the number of losses: "; getline(cin, convertor); cout << '\n'; holder.loses = stoi(convertor);
+        cout << "Enter the number of wins: "; getline(cin, convertor);  holder.wins = stoi(convertor);
+        cout << "Enter the number of losses: "; getline(cin, convertor);  holder.loses = stoi(convertor);
 
-        league.push_back(holder);
+        //Printing a buffer to help with readability
+        cout << "\n-------------------------\n";
 
+        //calculating the win/loss percentage and total games of the team
+        holder.totalGames();
+        holder.winLoss();
+
+        league.push_back(holder); //Adding the team to the vector
     }
+
+    for (size_t i = 0; i < league.size(); i++)
+    {
+        str = 0; //Reseting Str
+
+        for (size_t v = 0; v < league.size(); v++)
+        {
+            if (i != v)
+            {
+                str += league[v].record; //adding the record of each team to the Str variable
+            }
+        }
+        
+        //calculating the Schedule strength and adding it to the Struct stored in the vector
+        league[i].schStr = str / league.size();
+    }
+
+    for (size_t i = 0; i < league.size(); i++)
+    {
+        str = 0; //Reseting Str
+
+        for (size_t v = 0; v < league.size(); v++)
+        {
+            if (i != v)
+            {
+                str += league[v].schStr;
+            }
+        }
+        
+        //calculating the Schedule strength and adding it to the Struct stored in the vector
+        league[i].oppStr = str / league.size();
+    }
+
+    for (size_t i = 0; i < league.size(); i++)
+    {
+        cout << '\n' << league[i].name << "'s Win/Loss is " << league[i].record << "\nTheir Schedule Strength was " << league[i].schStr;
+        cout << "\nTheir Opponents Schedule Strenght was " << league[i].oppStr << "\nfor a total RPI of ";
+        cout << league[i].record * percent.winWeight + league[i].schStr * percent.strength + league[i].oppStr * percent.opponents;
+        cout << "\n-------------------------\n";
+    }
+    
     
     return 0;
 }
